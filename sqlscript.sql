779ac37767a0732c8210034a99b79b72b5675852
prgit21 <96674501+prgit21@users.noreply.github.com>
2022-01-04 20:52:38 +0530

    Add files via upload

---
+++ sqlscript.sql
@@ -0,0 +1,40 @@
+create table Colleges('Name' varchar(20),'duration' varchar(2),City varchar(20));
+
+insert into Colleges values ('Xyz',10,'ABC');
+
+insert into Colleges values ('RIT',04,'Blr');
+
+insert into Colleges values ('BMS',4,'Pune');
+
+create table Colleges(Name varchar(20),duration varchar(2),City varchar(20));
+
+insert into Colleges values ('Xyz',10,'ABC');
+
+insert into Colleges values ('RIT',04,'Blr');
+
+insert into Colleges values ('BMS',4,'Pune');
+
+create table Colleges(Name varchar(20),duration varchar(2),City varchar(20));
+
+insert into Colleges values ('Xyz',10,'ABC');
+
+insert into Colleges values ('RIT',04,'Blr');
+
+insert into Colleges values ('BMS',4,'Pune');
+
+select * from Colleges;
+
+create table Staff(College_Name varchar(20),Staff_name varchar(20),Designation varchar(20));
+
+insert into staff values ('BMS','Raju','Principal');
+
+insert into staff values ('BMS','Raja','Foreman');
+
+insert into staff values ('RIT','Rajesh','Principal');
+
+insert into staff values ('RIT','Ramu','HOD-CSE');
+
+select * from Colleges;
+
+select * from staff;
+
